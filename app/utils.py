1cda78d0d14c0d171c0c6d79ca96f7457f982087
h4cktivist <popovvadim0605@gmail.com>
2025-04-19 14:35:16 +0500

    feat: global index with combining add

--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,7 @@ from flask import current_app
 from werkzeug.datastructures import FileStorage
 
 import llama_index
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, load_index_from_storage, Settings
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, load_index_from_storage, Settings, ComposableGraph
 
 import uuid
 
@@ -46,10 +46,21 @@ def get_index_for_session(session_id):
         return session_indices[session_id]
     else:
         session_storage_dir = os.path.join(current_app.config['UPLOAD_FOLDER'], session_id)
+        global_storage_dir = os.path.join(current_app.config['UPLOAD_FOLDER'], 'global')
         if os.path.exists(session_storage_dir):
             print(f"Загружаем индекс с диска для сессии: {session_id}")
             storage_context = StorageContext.from_defaults(persist_dir=session_storage_dir)
-            index = load_index_from_storage(storage_context)
+            storage_global = StorageContext.from_defaults(persist_dir=global_storage_dir)
+
+            index_global = load_index_from_storage(storage_global)
+            index_user = load_index_from_storage(storage_global)
+            graph = ComposableGraph.from_indices(
+                VectorStoreIndex,
+                [index_global, index_user],
+                index_summaries=[index_global.summary, index_user.summary]
+            )
+
+            index = graph
             session_indices[session_id] = index
             return index
         else:
@@ -60,7 +71,8 @@ def query_index(session_id, query):
     index = get_index_for_session(session_id)
     if index:
         query_engine = index.as_query_engine()
-        response = query_engine.query(query)
+        response = query_engine.query('Ответь на следующий вопрос полностью на русском языке, со всеми подробностями '
+                                      'и исходя из содержания переданного тебе документа. ' + query)
         return response
     else:
         return None
@@ -71,16 +83,13 @@ def make_response(session_id, request: str, filename=None) -> dict:
         if session_id not in session_indices:
             load_document_and_create_index(session_id,
                                            os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-        response = {
-            'session_id': session_id,
-            'filename': filename,
-            'response': query_index(session_id, request).response
-        }
+        response_text = query_index(session_id, request).response
     else:
-        index = query_index(session_id, request)
-        response = {
-            'session_id': session_id,
-            'filename': filename,
-            'response': index.response if index is not None else Settings.llm.complete(request).text
-        }
-    return response
+        response_text = query_index(session_id, request)
+        response_text = response_text.response if response_text is not None else Settings.llm.complete(request).text
+
+    return {
+        'session_id': session_id,
+        'filename': filename,
+        'response': response_text
+    }
